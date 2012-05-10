79c8b59de15c28fbcc1161b7760ae1e226654ba5
Bug273 <fvargasgrc@gmail.com>
2012-05-10 13:51:23 +0200

    Tinymce configurado + styles

--- blogpost.php
+++ blogpost.php
@@ -34,6 +34,27 @@
   <link rel="stylesheet" href="style.css"/>
   <link rel="shortcut icon" type="image/x-icon"  href="images/favicon.ico" />
   <title>Blog Post</title>
+  <script type="text/javascript" src="tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
+    <script language="javascript" type="text/javascript">
+      tinyMCE.init({
+        mode: "textareas", 
+          /* theme: "simple", */
+        theme: "advanced",
+        theme_advanced_buttons1 : "bold,italic,underline,|,justifyleft,justifycenter,justifyright,fontselect,fontsizeselect",
+        theme_advanced_buttons2 : "cut,copy,paste,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image, media",
+        theme_advance_toolbar_location : "top",
+        theme_advanced_toolbar_align : "left",
+        theme_advanced_statusbar_location: "bottom",
+        theme_advanced_resizing: true, 
+
+        template_external_list_url : "lists/template_list.js",
+        external_link_list_url : "lists/link_list.js",
+        external_image_list_url : "lists/image_list.js",
+        media_external_list_url : "lists/media_list.js",
+
+        skin_variant: "silver",
+        width: "450"}); 
+    </script>
 </head>
 <body>
   <div id="contentarea">
@@ -43,13 +64,16 @@
       </div>
       <?php if ($action === 'show_form'): ?>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <h4>Titulo</h3>
+        <!-- <h4>Titulo</h3> -->
         <p><input type="text" name="title" id="title" /></p>
         <h4>Contenido</h3>
         <textarea name="content" rows="20"></textarea>
         <h5>Tags</h4>
         <p><input type="text" name="tags" id="tags" /></p>
-        <p><input type="submit" name="btn_submit" value="Salvar" /></p>
+        <div id="botoiak">
+          <p><input type="submit" name="btn_submit" value="Salvar" />
+          <input type="reset" name="btn_reset" value="Borrar" /> </p>
+        </div>
         </form>
       <?php else: ?>
         <p>Articulo guardado. _id:<?php echo $article['_id']; ?>. <br>
