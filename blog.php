46f73659b8e6d0e30714a0b398adcf2a0f5b169c
Bug273 <fvargasgrc@gmail.com>
2012-05-10 09:37:08 +0200

    Tinymce added

--- blog.php
+++ blog.php
@@ -29,7 +29,9 @@
       <h2><?php echo $article['title']; ?></h2>
       <p><?php echo $article['content']; ?></p>
     </div>
-    
+    <div class="volver">
+      <a href="http://blog.bug273.net/blogs.php">Volver</a>
+    </div>
     <div id="disqus_thread"></div>
     <script type="text/javascript">
       var disqus_shortname='bug273';
@@ -41,6 +43,12 @@
     </script>
       <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments.</a></noscript>
   </div>
+  <div id="footer">
+    <ul id="footer-logos">
+      <p><h4>Hecho con:&nbsp;</h4></p>
+      <li><a href="http://www.mongodb.org/"><img src="images/mongodb-logo.png" alt="Mongodb" width="80" height="50" /></a></li>
+      <li><a href="http://php.net/"><img src="images/php-logo.jpg" alt="PHP" width="80" height="50"  /></a></li>
+    </ul>
 </body>
 </html>
 
